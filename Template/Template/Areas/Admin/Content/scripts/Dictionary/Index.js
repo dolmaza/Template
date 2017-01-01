b50ed5a45a8394166418c1c29cc87b2123b9086d
Giorgi Dolmazashvili <giorgi.dolmazashvili@gmail.com>
2017-01-01 20:51:04 +0400

    add collapse expand buttons

--- Template/Template/Areas/Admin/Content/scripts/Dictionary/Index.js
+++ Template/Template/Areas/Admin/Content/scripts/Dictionary/Index.js
@@ -3,4 +3,21 @@
         DictionariesTree.StartEditNewNode();
         return false;
     });
+
+    $("#collapse-all-nodes")
+        .click(function () {
+            DictionariesTree.CollapseAll();
+
+            $(this).attr("disabled", "disabled");
+            $("#expand-all-nodes").removeAttr("disabled");
+
+        });
+
+    $("#expand-all-nodes")
+        .click(function () {
+            DictionariesTree.ExpandAll();
+
+            $(this).attr("disabled", "disabled");
+            $("#collapse-all-nodes").removeAttr("disabled");
+        });
 });
--- Template/Template/Areas/Admin/Content/scripts/Permission/Index.js
+++ Template/Template/Areas/Admin/Content/scripts/Permission/Index.js
@@ -1,6 +1,23 @@
 ﻿$(function () {
     $("#add-new").click(function () {
-        PermissionTree.StartEditNewNode();
+        PermissionsTree.StartEditNewNode();
         return false;
     });
+
+    $("#collapse-all-nodes")
+        .click(function() {
+            PermissionsTree.CollapseAll();
+
+            $(this).attr("disabled", "disabled");
+            $("#expand-all-nodes").removeAttr("disabled");
+
+        });
+
+    $("#expand-all-nodes")
+        .click(function () {
+            PermissionsTree.ExpandAll();
+
+            $(this).attr("disabled", "disabled");
+            $("#collapse-all-nodes").removeAttr("disabled");
+        });
 });
